7909ff1ead7286b8103f4ebd01be53ceca813b37
Jeremy J Starcher <jeremy.starcher@gmail.com>
2019-04-07 15:52:52 -0400

    Added scoreline glyphs

--- graphics_gen.js
+++ graphics_gen.js
@@ -23,6 +23,7 @@ const glyphs = `
 ;       ↓;
 
 Digits:
+
 ! XXXXXX !
 ! XX  XX !
 ! XX  XX !
@@ -83,14 +84,154 @@ Digits:
 !     XX !
 ! XXXXXX !
 
+! XXXXXX !
+! X    X !
+! XXXXXX !
+! X    X !
+! X    X !
+
+L1s:
+!   XX    XXX    !
+! XXXX   X       !
+!   XX    XX     !
+!   XX      X    !
+! XXXXXX XXX     !
+
+L2s:
+! XXXXXX  XXX    !
+!     XX X       !
+! XXXXXX  XX     !
+! XX        X    !
+! XXXXXX XXX     !
+
+L3s:
+! XXXXXX  XXX    !
+!     XX X       !
+!   XXXX  XX     !
+!     XX    X    !
+! XXXXXX XXX     !
+
+L4s:
+! XX  XX  XXX    !
+! XX  XX X       !
+! XXXXXX  XX     !
+!     XX    X    !
+!     XX XXX     !
+
+L5s:
+! XXXXXX  XXX    !
+! XX     X       !
+! XXXXXX  XX     !
+!     XX    X    !
+! XXXXXX XXX     !
+
+L6s:
+! XXXXXX  XXX    !
+! XX     X       !
+! XXXXXX  XX     !
+! XX  XX    X    !
+! XXXXXX XXX     !
+
+TopSubtotal:
+!                !
+!     XXXXXX     !
+!                !
+!     XXXXXX     !
+!                !
+
+TopBonus:
+!      XXX  XXXX !
+!  X     X  X    !
+! XXX  XXX  XXXX !
+!  X     X     X !
+!      XXX  XXXX !
+
+
+L3k:
+! XXXXXX X  X    !
+!     XX X X     !
+!   XXXX XX      !
+!     XX X X     !
+! XXXXXX X  X    !
+
+L4k:
+! XX  XX X  X    !
+! XX  XX X X     !
+! XXXXXX XX      !
+!     XX X X     !
+!     XX X  X    !
+
+LSmallStraight:
+!  XXX    XXX    !
+! X      X       !
+!  XX     XX     !
+!    X      X    !
+! XXX    XXX     !
+
+LLargeStraight:
+! X       XXX    !
+! X      X       !
+! X       XX     !
+! X         X    !
+! XXXX   XXX     !
+
+LFullHouse:
+! XXXX    XXX    !
+! X      X       !
+! XX      XX     !
+! X         X    !
+! X      XXX     !
+
+LYahtzee:
+! X     X   XXX  !
+!  X   X   X   X !
+!   XXX    XXXXX !
+!    X     X   X !
+!    X     X   X !
+
+LChance:
+! XXXX   XXX     !
+! X     X   X    !
+! X     XXXXX    !
+! X     X   X    !
+! XXXX  X   X    !
+
+LYahtzeeBonus:
+! X     X  XXXX  !
+!  X   X   X   X !
+!   XXX    XXXX  !
+!    X     X   X !
+!    X     XXXX  !
+
+LLowerTotal:
+!  X       XXXXX !
+!  X         X   !
+!  X         X   !
+!  X         X   !
+!  XXXXXX    X   !
+
+LUpperTotal:
+!  X    X  XXXXX !
+!  X    X    X   !
+!  X    X    X   !
+!  X    X    X   !
+!  XXXXXX    X   !
+
+LGrandTotal:
+!  XXXXXX  XXXXX !
+!  X         X   !
+!  X  XXX    X   !
+!  X    X    X   !
+!  XXXXXX    X   !
 
 `;
 
 const fs = require('fs');
 let glyph = [];
 let assembly = [];
+let glyphId = 0;
 
-function lineToBinary(line, isReverse) {
+function lineToBinary(line, lineNo, isReverse) {
   const digitZero = isReverse ? "0" : "1";
   const digitOne = isReverse ? "1" : "0";
 
@@ -99,33 +240,38 @@ function lineToBinary(line, isReverse) {
     .replace(/ /g, digitZero)
     .replace(/X/g, digitOne);
 
-  const s = `    .BYTE %` + binary;
+    if (binary.length !== 8 && binary.length !== 16) {
+      throw new Error(`Length of line is off on ine ${lineNo}: ${line}`);
+    }
+
+  const s = `    .BYTE %${binary}   ; ${binary.length} - ${line.length}`;
   glyph.push(s);
 }
 
-function title(line) {
-  lineToBinary(line, true);
+function title(line, lineNo) {
+  lineToBinary(line, lineNo, false);
 }
 
-function digit(line) {
-  lineToBinary(line, true);
+function digit(line, lineNo) {
+  lineToBinary(line, lineNo, true);
 }
 
 function normal(line) {
   if (glyph.length) {
-    assembly = assembly.concat(glyph.reverse());
+    glyphId++;
   }
 
-  glyph = [];
-
+  assembly = assembly.concat(glyph.reverse());
   assembly.push(line);
+
+  glyph = [];
 }
 
-glyphs.split(/\r\n|\r|\n/).forEach((line) => {
+glyphs.split(/\r\n|\r|\n/).forEach((line, lineNo) => {
   if (line[0] === '|') {
-     title(line);
+     title(line, lineNo);
   } else if (line[0] === "!") {
-     digit(line);
+     digit(line, lineNo);
   } else {
      normal(line);
   }
